d9e432c9dd9f7f3dcac93be1c598ce6f1f513f28
Kiruthikaece <102378821+Kiruthikaece@users.noreply.github.com>
2024-03-22 10:33:11 +0530

    Update minTimeToVisitAllPoints.java

--- leetcode_pbms/minTimeToVisitAllPoints.java
+++ leetcode_pbms/minTimeToVisitAllPoints.java
@@ -3,14 +3,14 @@ class Solution {
           int totalTime = 0;
 
         for (int i = 1; i < points.length; i++) {
-            int prevX = points[i - 1][0];
-            int prevY = points[i - 1][1];
+            int pX = points[i - 1][0];
+            int pY = points[i - 1][1];
 
-            int currX = points[i][0];
-            int currY = points[i][1];
+            int cX = points[i][0];
+            int cY = points[i][1];
 
-            int dx = Math.abs(currX - prevX);
-            int dy = Math.abs(currY - prevY);
+            int dx = Math.abs(cX - pX);
+            int dy = Math.abs(cY - pY);
 
             totalTime += Math.max(dx, dy);
         }
